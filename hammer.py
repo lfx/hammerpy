1044d25a2284f774a12aaacebc8471d35d02886f
Liudas Sodonis <liudas@sodonis.lt>
2016-01-01 21:57:25 -0800

    Added try except block to catch Keyboard interuptions

--- hammer.py
+++ hammer.py
@@ -20,27 +20,39 @@ def record(word):
         with open(args.out, "a") as tmp:
             tmp.write(word)
 
+def footer():
+    percent = good/total * 100
+    print("\nStat: total: {0}, good: {1}, bad: {2}. - {3:.0f}% to be correct!".format(total, good, bad, percent))
+
+
 if __name__ == "__main__":
     total = 0
     good = 0
     bad = 0
-    for line in lines:
-        total+=1
-        en = line.split(";")[0]
-        lt = line.split(";")[1]
-        print("{0} {1}".format(total, lt))
-        r = input("en: ")
-        if(en == r):
-            good+=1
-            print("Correct!")
-        else:
-            bad+=1
-            record(line)
-            print("Wrong! Should be - ", en)
-            lines.append(line)
-            while True:
-                rr = input("again: ")
-                if(en == rr):
-                    break
-    percent = good/total * 100
-    print("Stat: total: {0}, good: {1}, bad: {2}. - {3:.0f}% to be correct!".format(total, good, bad, percent))
+    try:
+        for line in lines:
+            total+=1
+            en = line.split(";")[0]
+            lt = line.split(";")[1]
+            print("{0} {1}".format(total, lt))
+            candidate = input("en: ")
+            if(en == candidate):
+                good+=1
+                print("Correct!")
+            else:
+                bad+=1
+                record(line)
+                print("Wrong! Should be - ", en)
+                lines.append(line)
+                while True:
+                    repeat_candidate = input("again: ")
+                    if(en == repeat_candidate):
+                        break
+
+    except KeyboardInterrupt:
+        pass
+    finally:
+        footer()
+
+    # percent = good/total * 100
+    # print("Stat: total: {0}, good: {1}, bad: {2}. - {3:.0f}% to be correct!".format(total, good, bad, percent))
